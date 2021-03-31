af9d5a90377af9f9a46938582975792620fe9224
Ericspk33 <ericspk33@gmail.com>
2021-03-30 18:56:41 -0700

    comment tue out and add usaco

--- app.js
+++ app.js
@@ -11,15 +11,15 @@ client.on('ready', () => {
     client.user.setActivity('bit.ly/compscilinktree', { type: 'WATCHING' });
 
     // Tuesday 3:30pm
-    var tue330 = new CronJob(
-      '0 30 15 * * 2',
-      function(){
-          client.channels.cache.get(announcement).send(`Hello @everyone,\n\nMeeting at lunch (12:35) tomorrow! We will be using Python to create algorithms to beat other bots in a rock paper scissors round-robin tournament.\n\nZoom Link: https://bit.ly/whscompscizoom\nPassword: **compsci**\n\nSee you there!`);
-      },
-      null,
-      true,
-      'America/Los_Angeles'
-    );
+    // var tue330 = new CronJob(
+    //   '0 30 15 * * 2',
+    //   function(){
+    //       client.channels.cache.get(announcement).send(`Hello @everyone,\n\nMeeting at lunch (12:35) tomorrow! We will be using Python to create algorithms to beat other bots in a rock paper scissors round-robin tournament.\n\nZoom Link: https://bit.ly/whscompscizoom\nPassword: **compsci**\n\nSee you there!`);
+    //   },
+    //   null,
+    //   true,
+    //   'America/Los_Angeles'
+    // );
 
     // Wednesday 12:30pm
     var wed1230 = new CronJob(
@@ -31,6 +31,11 @@ client.on('ready', () => {
         true,
         'America/Los_Angeles'
       );
+
+    // USACO start
+    // client.channels.cache.get(announcement).send(`@everyone US Open has opened ...`);
+    // USACO channel closure message
+    // client.channels.cache.get("752339796340244602").send(`Channel Locked 🔒`);
 });
 
 
