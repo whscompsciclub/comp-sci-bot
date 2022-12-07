853db48f5fa17ef2e9257a7b0dffd04dd83b13bf
Eric Wang <38512469+Ericspk33@users.noreply.github.com>
2022-12-07 07:32:27 -0800

    Update app.js

--- app.js
+++ app.js
@@ -173,7 +173,7 @@ client.on('ready', () => {
         '0 45 11 * * 2', // 0 0 19 * * 0
         function(){
             
-            client.channels.cache.get(announcement).send(`Hi @everyone! **Sorry NO Meeting today** (Mr. Blackford's not here). Happy Holidays! See you next year!!!`);
+            // client.channels.cache.get(announcement).send(`Hi @everyone! **Sorry NO Meeting today** (Mr. Blackford's not here). Happy Holidays! See you next year!!!`);
         },
         null,
         true,
